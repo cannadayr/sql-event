f602bc9a2d1bdbcd6cc3ce2fd549b3510ee1ec2e
Rowan Cannaday <cannadayr@gmail.com>
2018-11-06 00:34:28 -0600

    add sample data

---
+++ sample_data.sql
@@ -0,0 +1,5 @@
+insert into entity default values;
+-- insert into event (start_time,end_time,entity_id) values ('2018-01-01 00:00:00','2018-01-01 00:00:00',1);
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 00:00:00','2018-01-01 01:00:00',1);
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 02:00:00','2018-01-01 03:00:00',1);
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 04:00:00','2018-01-01 05:00:00',1);
