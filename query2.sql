055e982b26b15d426884e12975a8b26c3d479ccf
Rowan Cannaday <cannadayr@gmail.com>
2018-11-06 11:38:37 -0600

    minor cleanup for recursive time_range

--- query2.sql
+++ query2.sql
@@ -1,7 +1,14 @@
-with recursive time_range(time_ref) as (
-    select time_ref from start_moment    
+with recursive time_range(this_time_ref) as (
+    select
+        (select time_ref from start_moment)
     union all
-    select datetime(time_ref,(select rate from tick)) from time_range
+
+    select
+
+        datetime(this_time_ref,(select rate from tick))
+
+    from time_range
+
     limit (
         cast(strftime((select unit from tick_unit),(select time_ref from end_moment)) as integer)
         - cast(strftime((select unit from tick_unit),(select time_ref from start_moment)) as integer)
@@ -19,4 +26,5 @@ start_moment(time_ref) as (
 end_moment(time_ref) as (
     select '2018-01-01 02:00:00' as time_ref
 )
+
 select * from time_range;
