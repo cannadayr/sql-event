59ddbf0c461c9c885b90ae13af9287c88e7fe3bd
Rowan Cannaday <cannadayr@gmail.com>
2018-11-06 20:55:25 -0600

    add events for entity 2 to sample data

--- sample_data.sql
+++ sample_data.sql
@@ -3,3 +3,7 @@ insert into entity default values;
 insert into event (start_time,end_time,entity_id) values ('2018-01-01 00:00:00','2018-01-01 01:00:00',1);
 insert into event (start_time,end_time,entity_id) values ('2018-01-01 02:00:00','2018-01-01 03:00:00',1);
 insert into event (start_time,end_time,entity_id) values ('2018-01-01 04:00:00','2018-01-01 05:00:00',1);
+---
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 00:30:00','2018-01-01 01:30:00',2);
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 02:30:00','2018-01-01 03:30:00',2);
+insert into event (start_time,end_time,entity_id) values ('2018-01-01 04:30:00','2018-01-01 05:30:00',2);
