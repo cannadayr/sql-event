0ee2bac4b760210993ae3ff8ad39af46dde52a5e
Rowan Cannaday <cannadayr@gmail.com>
2018-11-06 19:46:50 -0600

    flesh out availability query
    left join over join
    coalesce prev/next events
    availability logic

--- query2.sql
+++ query2.sql
@@ -45,20 +45,45 @@ select
         from event
 
         where
-            entity_id = 1
+            entity_id = (select id from this_entity)
             and end_time < time_range.this_time_ref
 
         order by end_time desc
 
         limit 1
     ) as prev_event_id,
-    prev_event.start_time,
-    prev_event.end_time
+    prev_event.start_time as prev_event_start_time,
+    prev_event.end_time as prev_event_end_time,
+    (
+        select
+            id
+
+        from event
+
+        where
+            entity_id = (select id from this_entity)
+            and start_time > time_range.this_time_ref
+
+        order by start_time asc
+
+        limit 1
+    ) as next_event_id,
+    next_event.start_time as next_event_start_time,
+    next_event.end_time as next_event_end_time,
+    case when
+        ( -- #TODO calculate cleanup and setup times from previous/current event
+            datetime(time_range.this_time_ref,"-600 seconds") >= coalesce(prev_event.end_time,'1970-01-01 00:00:00')
+            and datetime(datetime(time_range.this_time_ref,"+600 seconds"),(select duration from new_event)) <= coalesce(next_event.start_time,'9999-12-31 00:00:00')
+            and (select not count(id) from event where start_time <= time_range.this_time_ref and end_time >= time_range.this_time_ref)
+        )
+        then "available" else "unavailable"
+    end availability
 
 from
     time_range
 
-join event prev_event on prev_event.id = prev_event_id
+left join event prev_event on prev_event.id = prev_event_id
 
+left join event next_event on next_event.id = next_event_id
 ;
 
